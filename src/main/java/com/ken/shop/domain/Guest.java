0f54a4297bde69544b81e41cd042be899577eace
kenyinka <ken.okuneye@gmail.com>
2015-11-25 14:51:43 -0500

    Removing lombok! Will introduce once I set it up as part of the compile phase...

--- src/main/java/com/ken/shop/domain/Guest.java
+++ src/main/java/com/ken/shop/domain/Guest.java
@@ -1,7 +1,6 @@
 package com.ken.shop.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
@@ -20,7 +19,6 @@ public class Guest {
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(name = "id", unique = true)
     @JsonProperty
-    @Getter
     private String id;
 
     @JsonProperty
@@ -30,6 +28,18 @@ public class Guest {
     protected Guest() { }
 
     public Guest(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
--- src/main/java/com/ken/shop/domain/Store.java
+++ src/main/java/com/ken/shop/domain/Store.java
@@ -1,8 +1,7 @@
 package com.ken.shop.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ken.shop.domain.helper.Currency;
-import lombok.Getter;
+import com.ken.shop.domain.helper.Currency;;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -26,20 +25,17 @@ public class Store {
     @Column(name = "name", unique = true)
     private String name;
 
-    @Getter
     @JsonProperty(value = "availableCurrencies")
     @Column(name = "currencies")
     @ElementCollection
     @Enumerated(EnumType.STRING)
     private List<Currency> currencies;
 
-    @Getter
     @JsonProperty(value = "defaultCurrency")
     @Column(name = "currency", unique = true)
     @Enumerated(EnumType.STRING)
     private Currency currency;
 
-    @Getter
     @JsonProperty
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "store")
     private List<Item> items;
@@ -52,4 +48,36 @@ public class Store {
         this.currency = currency;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Currency> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(List<Currency> currencies) {
+        this.currencies = currencies;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
+
